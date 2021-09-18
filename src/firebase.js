103c108deee455eb027f8f478d3f60b61ef1d6a6
Vishnu ES <55495318+Vishnu-es@users.noreply.github.com>
2021-09-19 00:28:13 +0530

    navbar

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBGd_IxyLJsZ1Yg7IWTKUScDcOlEYyxyRk",
+  authDomain: "clone-b8bba.firebaseapp.com",
+  projectId: "clone-b8bba",
+  storageBucket: "clone-b8bba.appspot.com",
+  messagingSenderId: "90584136046",
+  appId: "1:90584136046:web:c2540d98426977d5fc297f",
+};
